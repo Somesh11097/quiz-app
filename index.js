8fcdcc65c020c95120fd916eb6bbf98fe22037a0
Somesh banerjee <32162479+Somesh11097@users.noreply.github.com>
2022-09-13 22:56:50 +0530

    Another commit

--- index.js
+++ index.js
@@ -42,11 +42,75 @@ const quizData = [
         d: "Application Programming Language",
         answer: "a"
     }
+];
+
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+
+let currentQuiz = 0;
+// let answer= undefined;
+
+let score = 0 ;
 
 
 
+loadQuiz();
 
 
 
+ function loadQuiz() {
+    const currentQuizData = quizData[currentQuiz];
 
-]
+
+    questionEl.innerText = currentQuizData.question;
+    
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+
+    
+ }
+
+ function getSelected(){
+    const answerELs = document.querySelectorAll(".answer");
+
+   answersEls.forEach(answerEl => {
+        // (answer.value);
+        if(answerEl.Checked){
+                return answerEl.id;
+        }
+   });
+
+        return undefined;
+   
+        
+
+ }
+
+ submitBtn.addEventListener("click" , ()=> {
+    currentQuiz++;
+
+   const answer =  getSelected();
+
+   if (answer){
+    if(currentQuiz < quizData.length){
+        loadQuiz();
+    }
+    else{
+        //TODO: show result
+        alert("Test Completed");
+    }
+
+
+   }
+
+   
+    
+
+ })
