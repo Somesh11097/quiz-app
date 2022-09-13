8109608de6cc0499a83d38d1acb6ff598b4480bc
Somesh banerjee <32162479+Somesh11097@users.noreply.github.com>
2022-09-13 14:18:26 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const quizData = [
+    {
+        question : "How old is Somesh",
+        a: "10",
+        b: "17",
+        c: "24",
+        d: "100",
+        correct:"c"
+    },
+    {
+        question: "What is the most used Programming Language in 2022",
+        a: "Java",
+        b:"Python",
+        c:"JavaScript",
+        d:"C",
+        answer: "a"
+    },
+    {
+        question: "Who is the Fastest Man in Dc Universe",
+        a:"Superman",
+        b:"Batman",
+        c:"Flash",
+        d:"Green Arrow",
+        answer: "c"
+
+    },
+
+    {
+        question: "What is the National Sports of India",
+        a:"Hockey",
+        b:"Football",
+        c:"Cricket",
+        d: "BasketBall",
+        answer: "a"
+    },
+
+    {
+        question:" What does HTML Stands For? ",
+        a: "Hypertext Markup Language",
+        b: "Cascading Style Sheet",
+        c: "Json Object Notation",
+        d: "Application Programming Language",
+        answer: "a"
+    }
+
+
+
+
+
+
+
+]
